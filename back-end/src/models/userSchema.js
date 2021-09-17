b12dbe0d72defeea8d384b31d4fb129b3abe4495
Bharath02 <bharath.janakiram@banfico.com>
2021-09-17 13:02:16 +0530

    added user schema using mongoose

---
+++ back-end/src/models/userSchema.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const UserSchema= new Schema({
+    email: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    passwordHash: {
+        type: String
+    },
+    info: {
+        hairColor: {
+            type: String
+        },
+        favouriteFood: {
+            type: String
+        },
+        bio: {
+            type: String
+        }
+    },
+    isVerified: {
+        type: Boolean
+    }
+})
