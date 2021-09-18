85c5b6b28f091ecc4fd43d9e442fcf61654edcdf
Bharath02 <bharath.janakiram@banfico.com>
2021-09-18 09:59:16 +0530

    implemented update user route in backend

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -73,3 +73,52 @@ export const LoginController = async (req, res) => {
         });
     }
 }
+
+// Update users info Controller
+export const UpdateUsersController = async (req, res) => {
+    const { authorization } = req.headers;
+    const { userId } = req.params;
+
+    const updates = ({
+        favouriteFood,
+        hairColor,
+        bio
+    }) => ({
+        favouriteFood,
+        hairColor,
+        bio
+    }) (req.body);
+
+    if (!authorization) {
+        return res.status (401).json({ message: 'No authorization headers sent' });
+    }
+
+    const token = authorization.split (' ')[1];
+
+    jwt.verify (token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
+        if (err) {
+            return res.status (401).json ({ message: 'Unable to verify token' });
+        }
+
+        const { id } = decoded;
+
+        if (id !== userId) {
+            return res.status (403).json ({ message: "Not allowed to update other user's data" });
+        }
+
+        await User.findOneAndUpdate ({ _id: userId }, updates, { new: true, useFindAndModify: false }, (err, user) => {
+            if (err) {
+                return res.status (500).json ({message: `Failed to update user info ${err}`});
+            } else {
+                const { id, email, username, isVerified, info } = user;
+
+                jwt.sign ({ id, email, username, isVerified, info }, process.env.JWT_SECRET_KEY, { expiresIn: '2d' }, (err, token) => {
+                    if (err) {
+                        return res.status (500).json (err);
+                    }
+                    return res.status (200).json ({ token });
+                });
+            }
+        })
+    })
+}
--- back-end/src/routes/routeHandler.js
+++ back-end/src/routes/routeHandler.js
@@ -1,6 +1,7 @@
 import {
     SignupController,
-    LoginController
+    LoginController,
+    UpdateUsersController
 } from '../controllers/userController';
 
 const Routes = (app) => {
@@ -17,6 +18,10 @@ const Routes = (app) => {
     // Login route
     app.route ('/api/login')
         .post (LoginController)
+
+    // Update Users Info route
+    app.route ('/api/users/:userId')
+        .put (UpdateUsersController)
 };
 
 export default Routes;
